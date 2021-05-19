2af1b46f3697543e03433fc16b5de4ad956d24ed
seda-karadeniz <sedayo98@gmail.com>
2021-05-19 23:03:23 +0200

    aouter une rotation aux asteroids

--- src/Asteroid.js
+++ src/Asteroid.js
@@ -5,11 +5,15 @@ export default class Asteroid{
     constructor(canvas, ctx, parent= null) {
         this.canvas = canvas;
         this.ctx = ctx;
+        this.rotation = 0;
+
         if (!parent){
+            this.rotationSpeed = Math.random() /30;
             this.size = 7+ Math.random() * 5;
             this.location = new Vector(Math.random() * this.canvas.width, Math.random() * this.canvas.height  );
         }
         else {
+            this.rotationSpeed = parent.rotationSpeed * 2;
             this.size= parent.size/2;
             this.location = new Vector(parent.location.x, parent.location.y);
         }
@@ -35,6 +39,7 @@ export default class Asteroid{
     }
     update(){
         this.location.add(this.speed);
+        this.rotation += this.rotationSpeed;
         this.checkEdges();
         this.draw();
     }
@@ -57,7 +62,7 @@ export default class Asteroid{
     draw(){
         this.ctx.save();
         this.ctx.translate(this.location.x, this.location.y);
-        //this.ctx.rotate(this.heading);
+        this.ctx.rotate(this.rotation);
 
         this.ctx.fill(this.path);
 
